8fa0d17dc1e6b0e563bc220066a8c911feccce67
jonathon lee <jonnyleealas@gmail.com>
2020-12-09 20:09:21 -0600

    Toolbar works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+// import Paper from '@material-ui/core/Paper';
+import NavBar from './components/header/header.js';
 
 
 function App() {
   return (
     <>
-      <header>Hello World</header>
+      <NavBar />
+      <Button variant='contained'>Hello</Button>
     </>
   );
 }
