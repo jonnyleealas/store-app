a6914e780b455a68186857b3124619e2d9f7f3be
jonathon lee <jonnyleealas@gmail.com>
2020-12-09 21:13:48 -0600

    working code

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,18 +3,19 @@ import {AppBar,Toolbar, IconButton, Typography, Button} from  '@material-ui/core
 
 
 
+
 function NavBar (){
   return(
     <>
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start"  color="inherit" aria-label="menu">
-            {/* <MenuIcon /> */}
+        
           </IconButton> 
           <Typography variant="h6" >
           News 
           </Typography>
-          <Button color="inherit" >Login</Button>
+          <Button edge="end" color="inherit" >CART</Button>
         </Toolbar>
       </AppBar>
     </>
