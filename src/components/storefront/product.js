ddeb035133c535b8c251f0700e44dd39b7c3037c
EDGAR ROMERO <EDGAROMERO1987@GMAIL.COM>
2020-12-12 18:05:07 -0800

    updated list of products to display on click

--- src/components/storefront/product.js
+++ src/components/storefront/product.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { initialize } from '../../store/products.js'
 
+import Typography from '@material-ui/core/Typography';
+
 export default function Products() {
 
     const dispatch = useDispatch();
@@ -18,17 +20,36 @@ export default function Products() {
 
     return(
       <>
-        <h1>PRODUCT:</h1>
+        {/* <h1>PRODUCT:</h1> */}
+        {/* <Typography variant="h5">PRODUCTS:</Typography> */}
         <div>
-            {/* <p >{currentCategory}ACTIVE</p> */}
+            {/* <p >{currentCategory} ACTIVE</p> */}
             <ul>
                 {productsMap.map((item) => (
                 <li key={Math.random()}>
-                {item.name}
+                Name:{item.name}, 
+                Description: {item.description}
                 </li>
+                
+               
             ))}
             </ul> 
         </div>
+        <>
+       <ul>
+         {/* {props.displayProducts.map(product => { 
+          return( 
+          <li key={product.name}> 
+           {product.name}
+           {product.description}
+           {product.price}
+           {console.log('INVENTORY', product.inventory)}
+           <button onClick={() => props.addProductsToCart(product)}>Add to your Cart!</button>
+           </li> 
+             );
+        })} */}
+      </ul>
+      </>
       </>
     )
 }
